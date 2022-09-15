2d80deb60846d8da28b5ede3a8a150b367a8d4d6
kidusmik <kidusmik@gmail.com>
2022-09-15 20:40:27 +0300

    Add error handler for 404, 405 and 500

--- backend/src/api.py
+++ backend/src/api.py
@@ -151,13 +151,37 @@ Example error handling for unprocessable entity
 '''
 
 
-@app.errorhandler(422)
-def unprocessable(error):
+@app.errorhandler(404)
+def not_found(error):
     return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
+        'success': False,
+        'error': 404,
+        'message': 'requested resource not found'
+    }), 404
+
+@app.errorhandler(405)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'method not allowed'
+    }), 405
+
+@app.errorhandler(422)
+def unporcessable(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'unprocessable'
     }), 422
+
+@app.errorhandler(500)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'internal server error'
+    }), 500
 
 
 '''
