8d2dd97c698c458a77b9793d68432a9af9070628
kidusmik <kidusmik@gmail.com>
2022-09-15 20:34:14 +0300

    Add DELETE method for the drinks route

--- backend/src/api.py
+++ backend/src/api.py
@@ -122,8 +122,21 @@ def update_existing_drink(drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(drink_id):
+    drink = Drink.query.filter(Drink.id==drink_id).one_or_none()
 
+    if not drink:
+        abort(404)
 
+    drink.delete()
+
+    return jsonify({
+        'success': True,
+        'deleted': drink_id
+    })
+    
 # Error Handling
 '''
 Example error handling for unprocessable entity
